9520c3aa6d0edbef24928474eeafcb4ad65da6ae
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-01 05:38:10 +0100

    Styled UI Component

--- src/UI/Form.jsx
+++ src/UI/Form.jsx
@@ -3,7 +3,10 @@ import Input from "../Components/Input";
 
 export default function Form() {
   return (
-    <form action="#">
+    <form
+      action="#"
+      className="w-5/6 md:w-4/5 lg:w-3/6 m-auto flex flex-col gap-4"
+    >
       <ImageInput />
       <Input label="Full Name" id="full-name" type="text" name="full-name" />
       <Input
@@ -20,7 +23,12 @@ export default function Form() {
         name="github"
         placeholder="@yourusername"
       />
-      <button type="submit">Generate My Ticket</button>
+      <button
+        type="submit"
+        className="bg-orange-500 hover:bg-orange-600 text-slate-900 p-3 rounded-lg font-extrabold"
+      >
+        Generate My Ticket
+      </button>
     </form>
   );
 }
--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,11 +1,19 @@
 export default function Header() {
   return (
-    <header>
+    <header className="text-center">
       <div>
-        <img src="" alt="" />
+        <img
+          className="mx-auto my-8"
+          src="/src/assets/images/logo-full.svg"
+          alt="logo"
+        />
       </div>
-      <h1></h1>
-      <p></p>
+      <h1 className="text-5xl font-semibold">
+        Your Jounrney to Coding Conf 2025 Starts Here!
+      </h1>
+      <p className="text-xl my-6">
+        Secure your spot at this year&apos;s biggest coding conference.
+      </p>
     </header>
   );
 }
