f8fb862a2399b6b81ad0a47f7c1827a0b2160318
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-02 20:07:30 +0100

    Fixed image import for proper UI

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,12 +1,9 @@
+import logo from "../assets/images/logo-full.svg";
 export default function Header() {
   return (
     <header className="text-center">
       <div>
-        <img
-          className="mx-auto my-8"
-          src="/src/assets/images/logo-full.svg"
-          alt="logo"
-        />
+        <img className="mx-auto my-8" src={logo} alt="logo" />
       </div>
       <h1 className="text-5xl font-semibold">
         Your Jounrney to Coding Conf 2025 Starts Here!
