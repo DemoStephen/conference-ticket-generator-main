7b1caa5e42ea0c191d45af7333551478d7b70af9
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-02 09:53:42 +0100

    Trying to fix output page

--- src/UI/Output.jsx
+++ src/UI/Output.jsx
@@ -1,21 +1,28 @@
+import shortLogo from "../assets/images/logo-mark.svg";
+import avater from "../assets/images/image-avatar.jpg";
+import github from "../assets/images/icon-github.svg";
 export default function Output() {
   return (
-    <section className="bg-pattern-ticket bg-no-repeat bg-cover">
-      <div>
-        <article>
-          <img src="" alt="logo" />
+    <section className="p-6 bg-pattern-ticket bg-no-repeat">
+      <div className="flex flex-col gap-20">
+        <article className="flex items-center gap-4">
+          <img src={shortLogo} alt="logo" />
           <header>
-            <h3>Coding Conf</h3>
-            <p>Jan 31, 2025 / Austin, TX</p>
+            <h3 className="text-3xl font-bold">Coding Conf</h3>
+            <span>Jan 31, 2025 / Austin, TX</span>
           </header>
         </article>
-        <article>
-          <img src="" alt="logo" />
+        <article className="flex items-center gap-3">
+          <img className="w-14 h-14 rounded-md" src={avater} alt="logo" />
           <header>
-            <h4>{/* Dynamically update name */}</h4>
-            <p>
-              <img src="" alt="github" />
-              <span>@{/* Dynamically update username */}</span>
+            <h4 className="text-xl font-semibold">
+              Jonathan Kristof{/* Dynamically update name */}
+            </h4>
+            <p className="flex items-center gap-3">
+              <img src={github} alt="github" />
+              <span>
+                @jonathankristof0101{/* Dynamically update username */}
+              </span>
             </p>
           </header>
         </article>
