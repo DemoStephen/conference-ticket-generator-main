3a5278c72bc8dada598c0d3840ff27f2c392ed2f
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-24 23:15:11 +0100

    Inputs updated.

--- src/Components/ImageInput.jsx
+++ src/Components/ImageInput.jsx
@@ -12,34 +12,32 @@ export default function ImageInput() {
   };
 
   return (
-    <>
-      <fieldset>
-        <label htmlFor="upload-image" className="w-full font-bold block mb-1">
-          Upload Avatar
-        </label>
-        <input
-          type="file"
-          name="upload-image"
-          id="upload-image"
-          onChange={handleImageUpload}
-          className="hidden"
+    <fieldset>
+      <label htmlFor="upload-image" className="w-full font-bold block mb-1">
+        Upload Avatar
+      </label>
+      <input
+        type="file"
+        name="upload-image"
+        id="upload-image"
+        onChange={handleImageUpload}
+        className="hidden"
+      />
+      <label
+        htmlFor="upload-image"
+        className="block text-center p-4 border border-dashed rounded-lg mb-2"
+      >
+        <img
+          className="p-2 rounded-md mx-auto mb-4 border w-1/5"
+          src={imageUrl ?? upload}
+          alt="upload icon"
         />
-        <label
-          htmlFor="upload-image"
-          className="block text-center p-4 border border-dashed rounded-lg mb-2"
-        >
-          <img
-            className="p-2 rounded-md mx-auto mb-4 border w-1/5"
-            src={imageUrl ?? upload}
-            alt="upload icon"
-          />
-          <p>Drag and drop or click to upload</p>
-        </label>
-        <p className="flex items-center gap-2">
-          <img src={info} alt="info" />
-          <small>Upload your photo (JPG or PNG, max size: 500KB).</small>
-        </p>
-      </fieldset>
-    </>
+        <p>Drag and drop or click to upload</p>
+      </label>
+      <p className="flex items-center gap-2">
+        <img src={info} alt="info" />
+        <small>Upload your photo (JPG or PNG, max size: 500KB).</small>
+      </p>
+    </fieldset>
   );
 }
