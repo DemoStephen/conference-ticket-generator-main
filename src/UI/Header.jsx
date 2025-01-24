8fe45d2d1024a291ba72ff8fd251953cba0a5b97
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-24 23:14:36 +0100

    Redesigned header responsiveness

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,11 +1,11 @@
 import logo from "../assets/images/logo-full.svg";
 export default function Header() {
   return (
-    <header className="text-center md:w-3/5 px-8 md:px-0 m-auto">
+    <header className="text-center md:w-4/5 lg:w-3/5 px-8 md:px-0 m-auto">
       <div>
         <img className="mx-auto my-8" src={logo} alt="logo" />
       </div>
-      <h1 className="text-4xl md:text-5xl font-semibold">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold">
         Your Jounrney to Coding Conf 2025 Starts Here!
       </h1>
       <p className="text-xl my-6">
