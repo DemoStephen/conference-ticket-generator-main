28c231b21a2b7cfe5fc3deebb0306bf6b72ceb01
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-24 02:24:22 +0100

    fixed unrevelant issue with my image

--- src/Components/ImageInput.jsx
+++ src/Components/ImageInput.jsx
@@ -1,16 +1,31 @@
+import info from "../assets/images/icon-info.svg";
+import upload from "../assets/images/icon-upload.svg";
 export default function ImageInput() {
   return (
     <>
       <fieldset>
-        <label htmlFor="upload-image" className="w-full font-bold block mb-1">Upload Avatar</label>
-        <input type="file" name="upload-image" id="upload-image" className="hidden"/>
-        <label htmlFor="upload-image" className="block text-center p-4 border border-dashed rounded-lg mb-2">
-          <img className="p-2 rounded-md mx-auto mb-4 border" src="/src/assets/images/icon-upload.svg" alt="upload icon" />
+        <label htmlFor="upload-image" className="w-full font-bold block mb-1">
+          Upload Avatar
+        </label>
+        <input
+          type="file"
+          name="upload-image"
+          id="upload-image"
+          className="hidden"
+        />
+        <label
+          htmlFor="upload-image"
+          className="block text-center p-4 border border-dashed rounded-lg mb-2"
+        >
+          <img
+            className="p-2 rounded-md mx-auto mb-4 border"
+            src={upload}
+            alt="upload icon"
+          />
           <p>Drag and drop or click to upload</p>
         </label>
         <p className="flex items-center gap-2">
-          <img src="/src/assets/images/icon-info.svg" alt="info" />
-
+          <img src={info} alt="info" />
           <small>Upload your photo (JPG or PNG, max size: 500KB).</small>
         </p>
       </fieldset>
