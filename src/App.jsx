2fdfef3f8393966bba6a7d1cde993d512f744fb7
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-24 23:14:01 +0100

    I displayed all UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Form from "./UI/Form";
 import Header from "./UI/Header";
+import Output from "./UI/Output";
 
 function App() {
   return (
     <>
       <Header />
       <Form />
+      <Output />
     </>
   );
 }
