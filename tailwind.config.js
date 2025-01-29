25c48bfc51b62f95cab4fcc15aa576a3833fe6cb
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-29 18:39:18 +0100

    fixed utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    backgroundImage: {
-      "pattern-ticket": "url('/src/assets/images/pattern-ticket.svg')",
-      mobile:
-        "url('/src/assets/images/pattern-lines.svg'), url('/src/assets/images/pattern-squiggly-line-top.svg'), url('/src/assets/images/pattern-circle.svg'), url('/src/assets/images/pattern-squiggly-line-bottom.svg'), url('/src/assets/images/background-mobile.png')",
-      tablet:
-        "url('/src/assets/images/pattern-lines.svg'), url('/src/assets/images/pattern-squiggly-line-top.svg'), url('/src/assets/images/pattern-circle.svg'), url('/src/assets/images/pattern-squiggly-line-bottom.svg'), url('/src/assets/images/background-tablet.png')",
-      desktop:
-        "url('/src/assets/images/pattern-lines.svg'), url('/src/assets/images/pattern-squiggly-line-top.svg'), url('/src/assets/images/pattern-circle.svg'), url('/src/assets/images/pattern-squiggly-line-bottom.svg'), url('/src/assets/images/background-desktop.png')",
-    },
-    backgroundPosition: {
-      position: "top, top right, center, bottom left, center",
-      center: "center"
-    },
-    backgroundSize: {
-      desktopSize: "auto",
-      mobileSize: "auto, 60%, auto, 60%, auto",
-      contain: "contain"
-    },
-  },
+  theme: {},
   plugins: [],
 };
