d0bb2bed6b850d9ef40bcc697cddb8f399566fbe
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-29 18:45:30 +0100

    Updated Form

--- src/UI/Form.jsx
+++ src/UI/Form.jsx
@@ -3,10 +3,7 @@ import Input from "../Components/Input";
 
 export default function Form() {
   return (
-    <form
-      action="#"
-      className="w-5/6 md:w-4/5 lg:w-3/6 m-auto flex flex-col gap-4"
-    >
+    <form className="w-5/6 md:w-4/5 lg:w-3/6 m-auto flex flex-col gap-4">
       <ImageInput />
       <Input
         label="Full Name"
@@ -20,14 +17,14 @@ export default function Form() {
         id="email-name"
         type="email"
         name="email-name"
-        placeholder="example@email.com"
+        placeholder="codewithme.stephen@gmail.com"
       />
       <Input
         label="GitHub Username"
         id="github"
         type="text"
         name="github"
-        placeholder="@yourusername"
+        placeholder="@demostephen"
       />
       <button
         type="submit"
