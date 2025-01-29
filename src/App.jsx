bb74d33fe5df3fe3f374224f14a083ba3e2abcaf
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-29 18:44:42 +0100

    Added conditional statement for displaying the form and the output

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import Form from "./UI/Form";
 import Header from "./UI/Header";
 import Output from "./UI/Output";
 
-function App() {
+export default function App() {
+  const [info, setInfo] = useState({
+    image: null,
+    view: null,
+    name: "Stephen Adewale",
+    github: "@demostephen",
+    email: "codewithme.stephen@gmail.com",
+  });
+  function handleInfo([id, value]) {
+    setInfo({
+      ...prev,
+      id: [value],
+    });
+  }
   return (
     <>
-      <Header />
-      <Form />
-      <Output />
+      <Header data={info} />
+      {info.view ? <Output data={info} /> : <Form />}
     </>
   );
 }
-
-export default App;
